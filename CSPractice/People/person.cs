b31e6745f960f32c1f8f7ddf8240fc2d8a31f62f
Idowu Henry  Ogedengbe <ogedengbe_idowu@yahoo.co.uk>
2021-11-17 02:15:19 +0000

    Add project files.

---
+++ CSPractice/People/person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSPractice.people
+{
+        public class Person
+        {
+            public string Name;
+            public int Age;
+            public bool HasPet;
+
+            public void Greeting()
+            {
+                Console.WriteLine("Hi my Name is " + Name + " and my age is " + Age + " years");
+            }
+        }
+   
+}
---
+++ CSPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CSPractice.people;
+
+namespace CSPractice
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "idowu";
+            person.Age = 56;
+            person.HasPet = true;
+
+            person.Greeting();
+        }
+    }
+}
