a0fe9723317f8fafd7dde28917670f8c3eae9a1c
Hido <ogedengbe_idowu@yahoo.co.uk>
2021-11-22 21:31:51 +0000

    my first modification

---
+++ CSPractice/Sujects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSPractice
+{
+
+    public 
+    class Sujects
+    {
+
+        public int scores;
+        public bool pass;
+        public char grade;
+        public void Performance()
+        {
+            Console.WriteLine("My performance in this subject was great!!!");
+        }
+    }
+}
